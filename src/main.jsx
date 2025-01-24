22ae676537961fa369993dc63283e6f6f2fa3ade
Edmund <edmundgachanja@gmail.com>
2025-01-24 22:17:51 +0300

    Adds a custom error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ErrorPage from './components/ErrorPage.jsx'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element: <App /> 
+    path:'/,',
+    element: <App />,
+    errorElement: <ErrorPage/>
   }
 ])
 
